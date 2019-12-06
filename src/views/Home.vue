bfcf115425c28326a7924cfc765e246264011a66
yuanzengliang1999 <2455092892@qq.com>
2019-12-06 11:04:46 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-swipe @change="onChange">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+
+      <div class="custom-indicator" slot="indicator">{{ current + 1 }}/4</div>
+    </van-swipe>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import { Swipe, SwipeItem } from 'vant';
+
+Vue.use(Swipe).use(SwipeItem);
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      current: 0
+    };
+  },
+  methods: {
+    onChange(index) {
+      this.current = index;
+    }
   }
-}
+};
 </script>
